e84da8df544ad762e996411e0efcde1df30ce316
Electrostatus <electrostatus@gmail.com>
2017-10-28 19:03:06 -0400

    initial files

---
+++ analytic_asc.f90
@@ -0,0 +1,179 @@
+module analytic  ! ASCENDING ORDER!
+    implicit none
+    ! A collection of general purpose analytic formulas
+    ! for polynomials of degree 0 through 4 
+
+    ! Polynomials with degrees higher than four do not have analytic solutions
+
+    ! this file accepts the coefficients in ascending order: cof(n) /= 0
+    !    (cof(1) * x^0 + cof(2) * x^1 + cof(3) * x^2 + ...)
+    ! for coefficients in descending order, see 'analytic_dsc.f90'
+
+    integer, parameter  :: dp = selected_real_kind(8)  ! default precision
+    real(dp), parameter :: pi = 4.0_dp * atan(1.0_dp)
+
+    ! second and third cubic unity roots (first is just 1)
+    real(dp), parameter :: d60 = sin(pi / 3.0_dp), nd60 = -sin(pi / 3.0_dp)
+    complex(dp), parameter :: cu2 = (-0.5_dp, d60), cu3 = (-0.5_dp, nd60)
+
+    private
+    public :: root_0, root_1, root_2, root_3, root_4
+
+    contains
+        ! cof - complex array of coefficients,
+        !       in ascending order; cof(n) /= 0
+        ! rts - complex array of found roots
+        subroutine root_0(cof, rts)  ! constant,  a = 0
+            complex(dp), intent(in)  :: cof(:)
+            complex(dp), intent(out) :: rts(:)
+            rts(1) = 0.0_dp
+        end subroutine root_0
+
+        subroutine root_1(cof, rts)  ! linear,    b + ax = 0
+            complex(dp), intent(in)  :: cof(:)
+            complex(dp), intent(out) :: rts(:)
+            rts(1) = -cof(2) / cof(1)
+        end subroutine root_1
+
+        subroutine root_2(cof, rts)  ! quadratic, c + bx + ax^2 = 0
+            complex(dp), intent(in)  :: cof(:)
+            complex(dp), intent(out) :: rts(:)
+            complex(dp) :: p, q
+
+            p = sqrt(cof(2) * cof(2) - 4.0_dp * cof(1) * cof(3))
+            q = -2.0_dp * cof(3)
+            rts(1) = (cof(2) - p) / q
+            rts(2) = (cof(2) + p) / q
+        end subroutine root_2
+
+        subroutine root_3(cof, rts)  ! cubic,     d + cx + bx^2 + ax^3 = 0
+            complex(dp) :: abc, bbb, aad, dd, d0, d1, cin, cc, p, x1
+            complex(dp), intent(in)  :: cof(:)
+            complex(dp), intent(out) :: rts(:)
+
+            abc = cof(4) * cof(3) * cof(2)
+            bbb = cof(3) * cof(3) * cof(3)
+            aad = cof(4) * cof(4) * cof(1)
+
+            dd = 18.0_dp * abc * cof(1) - 4.0_dp * bbb * cof(1)
+            dd = dd + cof(3) * cof(3) * cof(2) * cof(2) - 4.0_dp * cof(4) * cof(2) * cof(2) * cof(2)
+            dd = dd - 27.0_dp * aad * cof(1)
+            d0 = cof(3) * cof(3) - 3.0_dp * cof(4) * cof(2)
+
+            if ((dd == 0).and.(d0 == 0)) then  ! triple root
+                x1 = -cof(3) / (3.0_dp * cof(4))
+                rts(1) = x1
+                rts(2) = x1
+                rts(3) = x1
+            else if ((dd == 0).and.(d0 /= 0)) then  ! double root, simple root
+                x1 = ((9.0_dp * cof(4) * cof(1) - cof(3) * cof(2)) / (2.0_dp * d0))
+                rts(1) = x1
+                rts(2) = x1
+                rts(3) = (4.0_dp * abc - 9.0_dp * aad - bbb) / (cof(4) * d0)
+            else
+                d1 = 2.0_dp * bbb - 9.0_dp * abc + 27.0_dp * aad
+
+                if (d0 == 0) then
+                    cin = d1
+                else
+                    cin = (d1 - sqrt(-27.0_dp * cof(4) * cof(4) * dd)) / 2.0_dp
+                end if
+                cc = cin ** (1.0_dp / 3.0_dp)
+                p = (-1.0_dp / (3.0_dp * cof(4)))
+
+                rts(1) = p * (cof(3) + cc + d0 / cc)
+                rts(2) = p * (cof(3) + cu2 * cc + d0 / (cu2 * cc))
+                rts(3) = p * (cof(3) + cu3 * cc + d0 / (cu3 * cc))
+            end if
+        end subroutine root_3
+
+        subroutine root_4(cof, rts)  ! quartic,   e + dx + cx^2 + bx^3 + ax^4 = 0
+            complex(dp) :: a, b, c, d, a2, b2, bq, c1
+            complex(dp) :: usq, usq2, u4, blkp, blkm
+            complex(dp) :: p1, v, u, uu, v3, p2, p3
+            complex(dp), intent(in)  :: cof(:)
+            complex(dp), intent(out) :: rts(:)
+
+            a = cof(4) / cof(5)
+            b = cof(3) / cof(5)
+            c = cof(2) / cof(5)
+            d = cof(1) / cof(5)
+
+            a2 = a * a; b2 = b * b
+
+            bq = -(2.0_dp * b2 * b) + 9.0_dp * a * b * c
+            bq = bq - 27.0_dp * (c * c + a2 * d) + 72.0_dp * b * d
+            c1 = b2 - 3.0_dp * a * c + 12.0_dp * d
+
+            p1 = sqrt(bq * bq - (4.0_dp * c1 * c1 * c1))
+            v = -(bq - p1) / 2.0_dp  ! solve quadratic
+            if (v == 0) v = -(bq + p1) / 2.0_dp  ! choose non zero quad root
+
+            u = a2 / 4.0_dp - (2.0_dp * b) / 3.0_dp
+            if (v == 0) then  ! both quad roots zero, uu simplifies to u
+                uu = u
+            else
+                v3 = (v ** (1.0_dp / 3.0_dp)) * cu2
+                uu = u + (1.0_dp / 3.0_dp) * (v3 + c1 / v3)
+            end if
+
+            p1 = - a / 4.0_dp
+
+            if (uu == 0) then  ! degenerate, quadruple root
+                rts(1) = p1
+                rts(2) = p1
+                rts(3) = p1
+                rts(4) = p1
+            else
+                p2 = 3.0_dp * a2 - 8.0_dp * b - 4.0_dp * uu
+                p3 = -(a2 * a) + 4.0_dp * a * b - 8.0_dp * c
+
+                usq = sqrt(uu)
+                usq2 = usq / 2.0_dp
+                u4 = uu / 4.0_dp
+
+                blkp = (1.0_dp / 4.0_dp) * sqrt(p2 + p3 / usq)
+                blkm = (1.0_dp / 4.0_dp) * sqrt(p2 - p3 / usq)
+
+                rts(1) = p1 + usq2 + blkp
+                rts(2) = p1 - usq2 + blkm
+                rts(3) = p1 + usq2 - blkp
+                rts(4) = p1 - usq2 - blkm
+            end if
+        end subroutine root_4
+
+end module analytic
+
+!program example
+!use analytic  ! ascending order
+!complex(8) :: cofs(5), rts(4)
+!
+!cofs = [3, 0, 0, 0, 0]
+!call root_0(cofs, rts)
+!print *,'root_0 in : ',cofs(1:1)
+!print *,'root_0 out: ', rts(1:1)
+!print *, ''
+!
+!cofs = [2, 3, 0, 0, 0]
+!call root_1(cofs, rts)
+!print *,'root_1 in : ',cofs(1:2)
+!print *,'root_1 out: ', rts(1:1)
+!print *, ''
+!
+!cofs = [7, 2, 3, 0, 0]
+!call root_2(cofs, rts)
+!print *,'root_2 in : ',cofs(1:3)
+!print *,'root_2 out: ', rts(1:2)
+!print *, ''
+!
+!cofs = [13, 7, 2, 3, 0]
+!call root_3(cofs, rts)
+!print *,'root_3 in : ',cofs(1:4)
+!print *,'root_3 out: ', rts(1:3)
+!print *, ''
+!
+!cofs = [5, 13, 7, 2, 3]
+!call root_4(cofs, rts)
+!print *,'root_4 in : ',cofs(1:5)
+!print *,'root_4 out: ', rts(1:4)
+!end program example
---
+++ analytic_funcs.py
@@ -0,0 +1,127 @@
+from cmath import sqrt
+__doc__ = """
+A collection of general purpose analytic formulas
+for polynomials of degree 0 through 4 
+
+Polynomials with degrees higher than four do not have analytic solutions
+"""
+
+def root_0(a):
+    """returns the roots for a constant equation
+    a = 0, polynomial of degree 0"""
+    return 0
+
+def root_1(a, b):
+    """returns the roots for a linear equation
+    ax + b = 0,  polynomial of degree 1"""
+    return -b / a
+
+def root_2(a, b, c):
+    """returns the roots for a quadratic equation
+    ax^2 + bx + c = 0,  polynomial of degree 2"""
+    p1 = sqrt(b * b - 4. * a * c)
+    p2 = -2. * a
+    
+    x1 = (b - p1) / p2
+    x2 = (b + p1) / p2
+    
+    return x1, x2
+
+def root_3(a, b, c, d):
+    """returns the roots for a cubic equation
+    ax^3 + bx^2 + cx + d = 0,  polynomial of degree 3"""
+    abc = a * b * c
+    bbb = b * b * b
+    aad = a * a * d
+
+    dd = (18. * abc * d - 4. * bbb * d
+          + b * b * c * c - 4. * a * c * c * c
+          - 27. * aad * d)
+    d0 = b * b - 3. * a * c
+
+    # second and third cubic unity roots (first is just 1)
+    cu2 = -0.5 + 0.86602540378443864676j
+    cu3 = -0.5 - 0.86602540378443864676j
+
+    if not dd and not d0:  # all real roots
+        x1 = x2 = x3 = -b / (3. * a)
+    elif not dd and d0:  # double root, simple root
+        x1 = x2 = ((9. * a * d - b * c) / (2. * d0))
+        x3 = (4. * abc - 9. * aad - bbb) / (a * d0)
+    else:
+        d1 = 2. * bbb - 9. * abc
+        d1 = d1 + 27. * aad
+
+        if not d0: cin = d1 + 0j # inner terms cancel
+        else: cin = (d1 - sqrt(-27.0 * a * a * dd)) / 2.
+
+        cc = cin ** (1. / 3.)
+        p = (-1. / (3. * a))
+
+        x1 = p * (b + cc + d0 / cc)
+        x2 = p * (b + u1 * cc + d0 / (cu2 * cc))
+        x3 = p * (b + u2 * cc + d0 / (cu3 * cc))
+
+    return x1, x2, x3
+
+def root_4(a, b, c, d, e):
+    """returns the roots for a quartic equation
+    ax^4 + bx^3 + cx^2 + dx + e = 0,  polynomial of degree 4"""
+    aa, bb, cc, dd = b / a, c / a, d / a, e / a
+    a2, b2 = aa * aa, bb * bb
+
+    bq = (- (2. * b2 * bb) + 9. * aa * bb * cc
+          - 27. * (cc * cc + a2 * dd)
+          + 72. * bb * dd)
+    c1 = (b2 - 3. * aa * cc + 12. * dd)
+    cu2 = -0.5 + 0.86602540378443864676j
+    
+    p1 = sqrt(bq * bq - (4. * c1 * c1 * c1))
+    v = (bq - p1) / -2.
+    if not v: v = (bq + p1) / -2.  # choose non zero quad root
+
+    u = a2 / 4. - (2. * bb) / 3.
+    if not v: uu = u  # both quad roots zero, uu simplifies to u
+    else:
+        v3 = (v ** (1. / 3.)) * cu2
+        uu = u + (1. / 3.) * (v3 + c1 / v3)
+
+    p1 = - aa / 4.
+    if not uu:  # degenerate, quadruple root
+        x1 = x2 = x3 = x4 = p1
+    else:
+        p2 = 3. * a2 - 8. * bb - 4. * uu
+        p3 = -(a2 * aa) + 4. * aa * bb - 8. * cc
+
+        usq = sqrt(uu)
+        usq2 = usq / 2.
+        u4 = uu / 4.
+
+        blkp = .25 * sqrt(p2 + p3 /  usq)
+        blkm = .25 * sqrt(p2 + p3 / -usq)
+
+        x1 = p1 + usq2 + blkp
+        x2 = p1 - usq2 + blkm
+        x3 = p1 + usq2 - blkp
+        x4 = p1 - usq2 - blkm
+    return x1, x2, x3, x4
+ 
+def cons(a):
+    "constant formula, returns root_0(a)"
+    return root_0(a)
+
+def lin(a, b):
+    "linear formula, returns root_1(a, b)"
+    return root_1(a, b)
+
+def quad(a, b, c):
+    "quadratic forumula, returns root_2(a, b, c)"
+    return root_2(a, b, c)
+
+def cubic(a, b, c, d):
+    "cubic forumula, returns root_3(a, b, c, d)"
+    return root_3(a, b, c, d)
+
+def quartic(a, b, c, d, e):
+    "quartic formula, returns root_4(a, b, c, d, e)"
+    return root_4(a, b, c, d, e)
